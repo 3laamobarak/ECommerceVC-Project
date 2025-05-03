99592e4508069b533d1163c0c9b448b52021334d
ayaalwarrdany <attallaa937@gmail.com>
2025-05-03 18:46:09 +0300

    add generic repository

--- ECommerceInfrastructure/GenericRepository.cs
+++ ECommerceInfrastructure/GenericRepository.cs
@@ -1,7 +1,42 @@
-﻿namespace ECommerceInfrastructure
-{
-    public class GenericRepository
-    {
+﻿using ECommerceContext;
+using Microsoft.EntityFrameworkCore;
 
+namespace ECommerceInfrastructure
+{
+    public class GenericRepository<T> where T : class
+    {
+        private readonly AppDBContext _appDbContext;
+        private readonly DbSet<T> _dbSet;
+        public GenericRepository(AppDBContext AppDbContext)
+        {
+            _appDbContext = AppDbContext;
+            _dbSet = _appDbContext.Set<T>();
+        }
+        public IQueryable<T> GetAll()
+        {
+            return _dbSet.AsNoTracking();
+        }
+        public async Task<T> CreateAsync(T entity)
+        {
+            var result = (await _dbSet.AddAsync(entity)).Entity;
+            await _appDbContext.SaveChangesAsync();
+            return result;
+        }
+        public async Task<T> UpdateAsync(T entity)
+        {
+            var result = _dbSet.Update(entity).Entity;
+            await _appDbContext.SaveChangesAsync();
+            return result;
+        }
+        public async Task<T> RemoveAsync(T entity)
+        {
+            var result = _dbSet.Remove(entity).Entity;
+            await _appDbContext.SaveChangesAsync();
+            return result;
+        }
+        public async Task<T?> GetByIdAsync(int Id)
+        {
+            return await _dbSet.FindAsync(Id);
+        }
     }
 }
